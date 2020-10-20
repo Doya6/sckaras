9b1cab92ef53afc55fdbe188dc975b889d358611
Jan Důjka <dujka72@gmail.com>
2020-10-20 13:42:06 +0200

    Create Login.vue

---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <NavBar/>
+        <!-- <h3 class='text-center light-blue lighten-3 white--text'>Přihlášení</h3> -->
+        <Login/>
+    </div>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue'
+import Formular from '@/components/Login.vue'
+export default {
+  name: 'Login',
+  components: {
+    NavBar,
+    Login
+  }
+}
+</script>
