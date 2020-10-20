e7fe861d5e2656ea85843fe8ec28a1e4e3bba940
Jan Důjka <dujka72@gmail.com>
2020-10-20 15:42:38 +0200

    Update Login.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-        <v-dialog v-model="dialog" persistent max-width="600px" min-width="360px">
+       <v-dialog v-model="dialog" persistent max-width="600px" min-width="360px">
             <div>
-                <v-tabs v-model="tab" show-arrows background-color="deep-purple accent-4" icons-and-text dark grow>
-                    <v-tabs-slider color="purple darken-4"></v-tabs-slider>
+                <v-tabs v-model="tab" show-arrows background-color="light-blue lighten-3" icons-and-text dark grow>
+                    <v-tabs-slider color="light-blue lighten-3"></v-tabs-slider>
                     <v-tab v-for="i in tabs" :key="i">
                         <v-icon large>{{ i.icon }}</v-icon>
                         <div class="caption py-1">{{ i.name }}</div>
@@ -17,7 +17,7 @@
                                             <v-text-field v-model="loginEmail" :rules="loginEmailRules" label="E-mail" required></v-text-field>
                                         </v-col>
                                         <v-col cols="12">
-                                            <v-text-field v-model="loginPassword" :append-icon="show1?'eye':'eye-off'" :rules="[rules.required, rules.min]" :type="show1 ? 'text' : 'password'" name="input-10-1" label="Password" hint="At least 8 characters" counter @click:append="show1 = !show1"></v-text-field>
+                                            <v-text-field v-model="loginPassword" :append-icon="show1?'eye':'eye-off'" :rules="[rules.required, rules.min]" :type="show1 ? 'text' : 'password'" name="input-10-1" label="Heslo" hint="At least 8 characters" counter @click:append="show1 = !show1"></v-text-field>
                                         </v-col>
                                         <v-col class="d-flex" cols="12" sm="6" xsm="12">
                                         </v-col>
@@ -68,53 +68,53 @@
 <script>
   export default {
     computed: {
-        passwordMatch() {
-          return () => this.password === this.verify || "Password must match"
-        }
-      },
-      methods: {
-        validate() {
-          if (this.$refs.loginForm.validate()) {
-            // submit form to server/API here...
-          }
-        },
-        reset() {
-          this.$refs.form.reset();
-        },
-        resetValidation() {
-          this.$refs.form.resetValidation();
-        }
-      },
-      data: () => ({
-        dialog: true,
-        tab: 0,
-        tabs: [
-            {name:"Login", icon:"mdi-account"},
-            {name:"Register", icon:"mdi-account-outline"}
-        ],
-        valid: true,
+    passwordMatch() {
+      return () => this.password === this.verify || "Heslo není platné";
+    }
+  },
+  methods: {
+    validate() {
+      if (this.$refs.loginForm.validate()) {
+        // submit form to server/API here...
+      }
+    },
+    reset() {
+      this.$refs.form.reset();
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation();
+    }
+  },
+  data: () => ({
+    dialog: true,
+    tab: 0,
+    tabs: [
+        {name:"Přihlášení", icon:"mdi-account"},
+        {name:"Registrace", icon:"mdi-account-outline"}
+    ],
+    valid: true,
+    
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    verify: "",
+    loginPassword: "",
+    loginEmail: "",
+    loginEmailRules: [
+      v => !!v || "Musí být vyplněno",
+      v => /.+@.+\..+/.test(v) || "E-mail není platný"
+    ],
+    emailRules: [
+      v => !!v || "Musí být vyplněno",
+      v => /.+@.+\..+/.test(v) || "E-mail není platný"
+    ],
 
-        firstName: "",
-        lastName: "",
-        email: "",
-        password: "",
-        verify: "",
-        loginPassword: "",
-        loginEmail: "",
-        loginEmailRules: [
-          v => !!v || "Required",
-          v => /.+@.+\..+/.test(v) || "E-mail must be valid"
-        ],
-        emailRules: [
-          v => !!v || "Required",
-          v => /.+@.+\..+/.test(v) || "E-mail must be valid"
-        ],
-
-        show1: false,
-        rules: {
-          required: value => !!value || "Required.",
-          min: v => (v && v.length >= 8) || "Min 8 characters"
-        }
-      })
-    };
+    show1: false,
+    rules: {
+      required: value => !!value || "Musí být vyplněno",
+      min: v => (v && v.length >= 8) || "Min 8 znaků"
+    }
+    })
+  };
   </script>
