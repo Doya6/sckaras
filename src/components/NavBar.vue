cad3bad823b821545e872639d94a8bf06913eed2
Jan Důjka <dujka72@gmail.com>
2020-11-02 14:54:51 +0100

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -292,9 +292,9 @@ export default {
         
         axios
         .post("https://mytestwww.tode.cz/SCKaras/sendMailRegConfirm.php", {
-          userName: "${this.userName}",
-          userPswd: "${this.password}",
-          userEmail: "${this.email}"
+          userName: ${this.userName},
+          userPswd: ${this.password},
+          userEmail: ${this.email}
         })
         alert(${this.email});
           this.userName = "";
