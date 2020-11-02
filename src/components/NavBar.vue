99d811934e4985e8650a0e03284b7981d1b67174
Jan Důjka <dujka72@gmail.com>
2020-11-02 14:47:12 +0100

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -294,8 +294,9 @@ export default {
         .post("https://mytestwww.tode.cz/SCKaras/sendMailRegConfirm.php", {
           //userName: '${this.userName}',
           //userPswd: '${this.password}',
-          userEmail: `'${this.email}'`
+          userEmail: "${this.email}"
         })
+        alert(${this.email});
           this.userName = "";
           this.phoneNumber = "";
           //this.email = "";
