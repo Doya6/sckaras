aa17fec6784884da4541d550fa048578a2ccf9a6
Jan Důjka <dujka72@gmail.com>
2020-11-03 13:26:13 +0100

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -206,8 +206,8 @@
                         <v-col class="d-flex ml-auto" cols="12" sm="6" xsm="12">
                           <v-switch
                             v-model="sendEmailSwitch"
-                            :label= "Přejete si odeslat přihlašovací údaje na e-mail? - " {{ odeslatEmail }}
-                          ></v-switch> 
+                            :label= "Přejete si odeslat přihlašovací údaje na e-mail?"
+                          > {{ odeslatEmail }} </v-switch> 
                         </v-col>    
                         <v-col class="d-flex ml-auto" cols="12" sm="4" xsm="12">
                           <v-btn
