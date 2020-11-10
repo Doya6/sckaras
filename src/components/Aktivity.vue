763e7e6f253b778b179980d47332edfc8db94ae6
Jan Důjka <dujka72@gmail.com>
2020-11-10 12:45:33 +0100

    Update Aktivity.vue

--- src/components/Aktivity.vue
+++ src/components/Aktivity.vue
@@ -70,15 +70,35 @@
       
       <v-card height="300px"
       class='px-auto mx-auto'
-      >
+      >      
       <div>
-          <h3 class='text-left pl-6 light-blue lighten-3 white--text py-3'>Moje rezervace</h3>
-          <h4 > {{ this.userID == '' ? "Nejdrive se prihlaste" : userID }} </h4>
-          
+          <h3 class='text-left pl-6 light-blue lighten-3 white--text py-3'>Moje rezervace</h3>          
           <p v-if="userID == ''" > Pro zobrazení vašich rezervací se musíte přihlásit. </p>
           <h4 v-else> userID: {{ userID }} </h4>
           
       </div>
+      <v-row
+          v-for="(myActivity, index) in listOfMyActivities"
+          v-bind:key="index"
+          class = "rows"
+          >
+          <v-col  cols="6" xs="5" sm="7" md="8" lg="9" class = "pl-2 pr-0" align="start">
+            <h5 class="mb-0" > {{ myActivity.eventStartDate.slice(0, -3) }}  - {{ myActivity.eventEndDate.slice(10, -3) }} hod</h5>
+            <p class="mb-0"> {{ myActivity.eventDesc }}</p>
+          </v-col>
+          <v-col  cols="2" xs="2" sm="2" md="2" lg="1" class = "pl-2 pr-0" align="center">
+           <p class = "my-0 hidden-xs-only"> obsazenost </p>
+           <p class = "my-0 hidden-sm-and-up"> obs. </p>
+           <p class = "my-0"> {{ myActivity.mySUM == null ? 0 :  myActivity.mySUM }}/{{ myActivity.maxSumOfAttendees }}</p>
+          </v-col>
+          <v-col  cols="4" xs="5" sm="2" md="2" lg="2" class = "pr-2" align="end">
+            <v-btn
+              depressed v-on:click=naCoSiKliknul(myActivity.myEventID)
+             >
+              Rezervovat2
+            </v-btn>
+          </v-col>
+        </v-row>
       </v-card>
       
     </div>
@@ -117,6 +137,7 @@ export default {
     listOfAktivityTypes: [],
     selectedActivityTypes: [],
     listOfActivities: [],
+    listOfMyActivities: [],
     
     datum: Date.now()    
     
@@ -141,6 +162,16 @@ export default {
       })
       .then((response) => {
           this.listOfActivities = (response.data);
+      });
+    },
+    
+    getMyAktivityList() {
+    axios
+      .post("https://mytestwww.tode.cz/SCKaras/selectMyEvents.php", {
+         sqlStringWhere: this.userID  
+      })
+      .then((response) => {
+          this.listOfMyActivities = (response.data);
       });
     },
       
