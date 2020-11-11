a3cc60058d6bd5280e5aa30ce7ba01bc0ee406a4
Jan Důjka <dujka72@gmail.com>
2020-11-11 10:53:10 +0100

    Update Aktivity.vue

--- src/components/Aktivity.vue
+++ src/components/Aktivity.vue
@@ -57,7 +57,7 @@
           <v-col  cols="4" xs="5" sm="2" md="2" lg="2" class = "pr-2" align="end">
             <v-btn
               depressed v-on:click=naCoSiKliknul(activity.myEventID)
-               v-bind:disabled="(activity.mySUM == activity.maxSumOfAttendees || this.userID === '')"
+               v-bind:disabled="((activity.mySUM == activity.maxSumOfAttendees) || (this.userID === ''))"
              >
               Rezervovat
             </v-btn>
