77e53c82007e62fdbb475c809ad1171ee1419cb2
Jan Důjka <dujka72@gmail.com>
2020-11-12 12:59:20 +0100

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+    <span class="bg"></span>
         <v-card 
           class='pa-5 mx-auto mt-15 text-center'
           width='500px'>
@@ -19,3 +20,17 @@ export default {
 }
 
 </script>
+
+<style>
+.bg {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: url( 'https://cdn.pixabay.com/photo/2016/03/26/13/09/organic-1280537_1280.jpg') no-repeat center center;
+    background-size: cover;
+    background-color: gray;
+    transform: scale(1.1);
+  }
+  </style>
