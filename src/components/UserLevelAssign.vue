f9ed9fb1ee48f9c4eb13bcf78b5385cc7281619f
Jan Důjka <dujka72@gmail.com>
2020-11-16 13:53:23 +0100

    Update UserLevelAssign.vue

--- src/components/UserLevelAssign.vue
+++ src/components/UserLevelAssign.vue
@@ -3,9 +3,12 @@
     
     <v-section v-if="this.userLevel === '1'">
       <h1 class='pl-6 justify="center" light-blue lighten-3 white--text'>Správa uživatelských přístupů UserLevel: {{ userLevel }}</h1>
-      <p>
-        list form
-      </p>
+      <v-autocomplete
+      filled
+      solo
+      v-bind:"items">Seznam
+      </v-autocomplete>
+      <v-btn v-on:click="show">SHOW</v-btn>
     </v-section>
     <p v-else>Pro vstup na tuto stránku nemáte oprávnění.</p>
     
@@ -17,13 +20,23 @@
 export default {
   mounted() {
     this.userLevel = this.$store.getters.getUserLevel;
+    
   },
+  
+  methods:(){
+    show(){
+    alert(this.items);
+    alert(this.values);
+    alert(this.value);
+    }
+  }
   
   data() {
     return {
-      userLevel: ''
-
-      
+      userLevel: '',
+      items: ['foo', 'bar', 'fizz', 'buzz'],
+      values: ['foo', 'bar'],
+      value: null,
     };
   },
 };
